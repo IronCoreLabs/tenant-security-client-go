5b07cb055e9930e3b5f17178c6f51a8a7cae272a
Chris Jones <chris.jones@ironcorelabs.com>
2022-05-20 11:03:35 -0600

    Add makeJsonRequest.

--- tenant_security_request.go
+++ tenant_security_request.go
@@ -11,6 +11,7 @@ import (
 
 const version = "0.1" // TODO Auto update this.
 
+// Paths that refer to TSP REST endpoints.
 const (
 	tsp_api_prefix_str             string = "/api/1/"
 	wrap_endpoint_str              string = "document/wrap"
@@ -21,63 +22,50 @@ const (
 	tenant_key_derive_endpoint_str string = "key/derive"
 	security_event_endpoint_str    string = "event/security-event"
 )
+
+var tsp_api_prefix *url.URL
+
+type tspEndpoint url.URL
+
+// These values can be used like an enum of valid TSP endpoints.
 var (
-	tsp_api_prefix             *url.URL
-	wrap_endpoint              *url.URL
-	batch_wrap_endpoint        *url.URL
-	unwrap_endpoint            *url.URL
-	batch_unwrap_endpoint      *url.URL
-	rekey_endpoint             *url.URL
-	tenant_key_derive_endpoint *url.URL
-	security_event_endpoint    *url.URL
+	wrap_endpoint              *tspEndpoint
+	batch_wrap_endpoint        *tspEndpoint
+	unwrap_endpoint            *tspEndpoint
+	batch_unwrap_endpoint      *tspEndpoint
+	rekey_endpoint             *tspEndpoint
+	tenant_key_derive_endpoint *tspEndpoint
+	security_event_endpoint    *tspEndpoint
 )
 
 func init() {
 	var err error
-
 	tsp_api_prefix, err = url.Parse(tsp_api_prefix_str)
 	if err != nil {
-		log.Panicf("Unable to parse tsp_api_prefix %q as relative URL: %e", tsp_api_prefix_str, err)
+		log.Panicf("Unable to parse tsp_api_prefix_str %q as relative URL: %e", tsp_api_prefix_str, err)
 	}
 
-	wrap_endpoint, err = url.Parse(wrap_endpoint_str)
-	if err != nil {
-		log.Panicf("Unable to parse wrap_endpoint %q as relative URL: %e", wrap_endpoint_str, err)
+	parseUrl := func(urlStr, name string) *tspEndpoint {
+		url, err := url.Parse(urlStr)
+		if err != nil {
+			log.Panicf("Unable to parse %s %q as relative URL: %e", name, urlStr, err)
+		}
+		return (*tspEndpoint)(url)
 	}
 
-	batch_wrap_endpoint, err = url.Parse(batch_wrap_endpoint_str)
-	if err != nil {
-		log.Panicf("Unable to parse batch_wrap_endpoint %q as relative URL: %e", batch_wrap_endpoint_str, err)
-	}
-
-	unwrap_endpoint, err = url.Parse(unwrap_endpoint_str)
-	if err != nil {
-		log.Panicf("Unable to parse unwrap_endpoint %q as relative URL: %e", unwrap_endpoint_str, err)
-	}
-
-	batch_unwrap_endpoint, err = url.Parse(batch_unwrap_endpoint_str)
-	if err != nil {
-		log.Panicf("Unable to parse batch_unwrap_endpoint %q as relative URL: %e", batch_unwrap_endpoint_str, err)
-	}
-
-	rekey_endpoint, err = url.Parse(rekey_endpoint_str)
-	if err != nil {
-		log.Panicf("Unable to parse rekey_endpoint %q as relative URL: %e", rekey_endpoint_str, err)
-	}
-
-	tenant_key_derive_endpoint, err = url.Parse(tenant_key_derive_endpoint_str)
-	if err != nil {
-		log.Panicf("Unable to parse tenant_key_derive_endpoint %q as relative URL: %e", tenant_key_derive_endpoint_str, err)
-	}
-
-	security_event_endpoint, err = url.Parse(security_event_endpoint_str)
-	if err != nil {
-		log.Panicf("Unable to parse security_event_endpoint %q as relative URL: %e", security_event_endpoint_str, err)
-	}
+	wrap_endpoint = parseUrl(wrap_endpoint_str, "wrap_endpoint")
+	batch_wrap_endpoint = parseUrl(batch_wrap_endpoint_str, "batch_wrap_endpoint")
+	unwrap_endpoint = parseUrl(unwrap_endpoint_str, "unwrap_endpoint")
+	batch_unwrap_endpoint = parseUrl(batch_unwrap_endpoint_str, "batch_unwrap_endpoint")
+	rekey_endpoint = parseUrl(rekey_endpoint_str, "rekey_endpoint")
+	tenant_key_derive_endpoint = parseUrl(tenant_key_derive_endpoint_str, "tenant_key_derive_endpoint")
+	security_event_endpoint = parseUrl(security_event_endpoint_str, "security_event_endpoint")
 }
 
+// tenantSecurityRequest is a long-lived object that sends and receives HTTP requests to the TSP.
 type tenantSecurityRequest struct {
-	apiKey     string
+	apiKey string
+	// Address of the TSP, including the API prefix
 	tspAddress *url.URL
 }
 
@@ -87,20 +75,16 @@ func newTenantSecurityRequest(apiKey string, tspAddress *url.URL) (*tenantSecuri
 	return req, nil
 }
 
+// wrapKey requests the TSP to generate a DEK and an EDEK.
 func (r *tenantSecurityRequest) wrapKey() (string, error) {
 	reqBody := io.NopCloser(strings.NewReader(`{"tenantId": "tenant-gcp", "iclFields": {"requestingId": "bar"}, "customFields": {}}`))
-	req, err := r.newRequest(r.tspAddress.ResolveReference(wrap_endpoint), reqBody)
+	resp, err := r.makeJsonRequest(wrap_endpoint, reqBody)
 	if err != nil {
 		return "", err
 	}
+	defer resp.Close()
 
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return "", err
-	}
-	// TODO Check response codes for errors.
-	defer resp.Body.Close()
-	respBody, err := io.ReadAll(resp.Body)
+	respBody, err := io.ReadAll(resp)
 	if err != nil {
 		return "", err
 	}
@@ -108,11 +92,16 @@ func (r *tenantSecurityRequest) wrapKey() (string, error) {
 	return string(respBody), nil
 }
 
-func (r *tenantSecurityRequest) newRequest(path *url.URL, body io.ReadCloser) (*http.Request, error) {
+// makeJsonRequest sends a JSON request body to a TSP endpoint and returns the response body. If the request can't be sent, or if
+// the server response code indicates an error, this function returns an error instead. Caller is responsible for closing the
+// response body.
+func (r *tenantSecurityRequest) makeJsonRequest(endpoint *tspEndpoint, reqBody io.ReadCloser) (io.ReadCloser, error) {
+	// Build the request.
+	url := r.tspAddress.ResolveReference((*url.URL)(endpoint))
 	req := http.Request{
-		URL: r.tspAddress.ResolveReference(path),
-		Body: body,
+		URL:    url,
 		Method: http.MethodPost,
+		Body:   reqBody,
 		Header: map[string][]string{
 			"User-Agent":    {fmt.Sprintf("Tenant Security Client Go %s", version)},
 			"Content-Type":  {"application/json"},
@@ -120,5 +109,18 @@ func (r *tenantSecurityRequest) newRequest(path *url.URL, body io.ReadCloser) (*
 			"Authorization": {fmt.Sprintf("cmk %s", r.apiKey)},
 		},
 	}
-	return &req, nil
+
+	// Perform the request.
+	resp, err := http.DefaultClient.Do(&req)
+	if err != nil {
+		return nil, fmt.Errorf("POST to %q: %e", url, err)
+	}
+
+	// Check the response code.
+	if resp.StatusCode >= 400 {
+		return nil, fmt.Errorf("POST to %q: %s", url, http.StatusText(resp.StatusCode))
+	}
+
+	// Return the body.
+	return resp.Body, nil
 }
--- tenant_security_request_test.go
+++ tenant_security_request_test.go
@@ -1,28 +1,54 @@
 package tenant_security_client_go
 
 import (
+	"fmt"
 	"io"
+	"net/http"
+	"net/http/httptest"
 	"net/url"
 	"strings"
 	"testing"
 )
 
-func TestHello(t *testing.T) {
-	url, err := url.Parse("http://localhost:1234")
+func TestMakeJsonRequest(t *testing.T) {
+	apiKey := "fake_key"
+	endpoint := wrap_endpoint
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/api/1/document/wrap" {
+			t.Errorf("request path %q", r.URL.Path)
+		}
+		authHeaders := r.Header["Authorization"]
+		if len(authHeaders) != 1 {
+			t.Fatalf("%d auth headers: %v", len(authHeaders), authHeaders)
+		}
+		authHeader := authHeaders[0]
+		if authHeader != "cmk "+apiKey {
+			t.Errorf("auth header %q", authHeader)
+		}
+
+		fmt.Fprintf(w, "{}")
+	})
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
+	url, err := url.Parse(server.URL)
 	if err != nil {
-		t.Errorf("%e", err)
+		t.Fatal(err)
 	}
-	r, err := newTenantSecurityRequest("key", url)
+
+	r, err := newTenantSecurityRequest(apiKey, url)
 	reqBody := io.NopCloser(strings.NewReader(`{}`))
-	reqUrl, err := url.Parse("foo")
-	if err != nil {
-		t.Error(err)
-	}
-	req, err := r.newRequest(reqUrl, reqBody)
+	resp, err := r.makeJsonRequest(endpoint, reqBody)
 	if err != nil {
 		t.Errorf("newRequest: %e", err)
 	}
-	if req.URL.Path != "/api/1/foo" {
-		t.Errorf("Wrong request path. Got %q, expected %q", req.URL.Path, "/api/1/foo")
+	defer resp.Close()
+	respBody, err := io.ReadAll(resp)
+	if err != nil {
+		t.Errorf("read response body: %e", err)
+	}
+	if string(respBody) != "{}" {
+		t.Errorf("response body: %q", respBody)
 	}
 }
