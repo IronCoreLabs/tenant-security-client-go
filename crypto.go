dbefb0a6dae6b4e6134cdffef7aa8afb7772c2ba
Chris Jones <chris.jones@ironcorelabs.com>
2022-05-27 10:45:24 -0600

    Merge the crypto package into tsc.

--- crypto.go
+++ crypto.go
@@ -1,4 +1,4 @@
-package crypto
+package tsc
 
 import (
 	"bytes"
@@ -46,15 +46,15 @@ func generateNonce() ([]byte, error) {
 	return nonce, nil
 }
 
-func Encrypt(plaintext []byte, key []byte) ([]byte, error) {
+func encrypt(plaintext []byte, key []byte) ([]byte, error) {
 	nonce, err := generateNonce()
 	if err != nil {
 		return nil, err
 	}
-	return EncryptWithNonce(plaintext, key, nonce)
+	return encryptWithNonce(plaintext, key, nonce)
 }
 
-func EncryptWithNonce(plaintext []byte, key []byte, nonce []byte) ([]byte, error) {
+func encryptWithNonce(plaintext []byte, key []byte, nonce []byte) ([]byte, error) {
 	if len(nonce) != nonceLen {
 		return nil, errors.New("the nonce passed was not the correct length")
 	}
@@ -65,19 +65,19 @@ func EncryptWithNonce(plaintext []byte, key []byte, nonce []byte) ([]byte, error
 	return gcm.Seal(nonce, nonce, plaintext, nil), nil
 }
 
-func EncryptDocument(document []byte, tenantID string, dek []byte) ([]byte, error) {
-	header, err := GenerateHeader(dek, tenantID)
+func encryptDocument(document []byte, tenantID string, dek []byte) ([]byte, error) {
+	header, err := generateHeader(dek, tenantID)
 	if err != nil {
 		return nil, err
 	}
-	encrypted, err := Encrypt(document, dek)
+	encrypted, err := encrypt(document, dek)
 	if err != nil {
 		return nil, err
 	}
 	return append(header, encrypted...), nil
 }
 
-func Decrypt(ciphertext []byte, key []byte) ([]byte, error) {
+func decrypt(ciphertext []byte, key []byte) ([]byte, error) {
 	if len(ciphertext) <= nonceLen+tagLen {
 		return nil, errors.New("the ciphertext was not well formed")
 	}
@@ -94,8 +94,8 @@ func Decrypt(ciphertext []byte, key []byte) ([]byte, error) {
 	return plaintext, nil
 }
 
-func DecryptDocument(document []byte, dek []byte) ([]byte, error) {
-	documentParts, err := SplitDocument(document)
+func decryptDocument(document []byte, dek []byte) ([]byte, error) {
+	documentParts, err := splitDocument(document)
 	if err != nil {
 		return nil, err
 	}
@@ -106,30 +106,30 @@ func DecryptDocument(document []byte, dek []byte) ([]byte, error) {
 		return nil, err
 	}
 	ciphertext := documentParts.ciphertext
-	if !VerifySignature(dek, &documentHeader) {
+	if !verifySignature(dek, &documentHeader) {
 		return nil, errors.New("the signature computed did not match. The document key is likely incorrect")
 	}
-	return Decrypt(ciphertext, dek)
+	return decrypt(ciphertext, dek)
 }
 
-func GenerateSignature(dek []byte, nonce []byte, header *icl_proto.SaaSShieldHeader) (*V3HeaderSignature, error) {
+func generateSignature(dek []byte, nonce []byte, header *icl_proto.SaaSShieldHeader) (*v3HeaderSignature, error) {
 	headerBytes, err := proto.Marshal(header)
 	if err != nil {
 		return nil, err
 	}
-	encryptedHeaderValue, err := EncryptWithNonce(headerBytes, dek, nonce)
+	encryptedHeaderValue, err := encryptWithNonce(headerBytes, dek, nonce)
 	if err != nil {
 		return nil, err
 	}
 	encryptedHeaderLength := len(encryptedHeaderValue)
 	tag := encryptedHeaderValue[encryptedHeaderLength-tagLen:]
-	return &V3HeaderSignature{tag, nonce}, nil
+	return &v3HeaderSignature{tag, nonce}, nil
 }
 
-func CreateHeaderProto(dek []byte, tenantID string, nonce []byte) (*icl_proto.V3DocumentHeader, error) {
+func createHeaderProto(dek []byte, tenantID string, nonce []byte) (*icl_proto.V3DocumentHeader, error) {
 	saasHeader := icl_proto.SaaSShieldHeader{}
 	saasHeader.TenantId = tenantID
-	signature, err := GenerateSignature(dek, nonce, &saasHeader)
+	signature, err := generateSignature(dek, nonce, &saasHeader)
 	if err != nil {
 		return nil, err
 	}
@@ -139,12 +139,12 @@ func CreateHeaderProto(dek []byte, tenantID string, nonce []byte) (*icl_proto.V3
 	return &v3Header, nil
 }
 
-func GenerateHeader(dek []byte, tenantID string) ([]byte, error) {
+func generateHeader(dek []byte, tenantID string) ([]byte, error) {
 	nonce, err := generateNonce()
 	if err != nil {
 		return nil, err
 	}
-	headerProto, err := CreateHeaderProto(dek, tenantID, nonce)
+	headerProto, err := createHeaderProto(dek, tenantID, nonce)
 	if err != nil {
 		return nil, err
 	}
@@ -169,39 +169,39 @@ func GenerateHeader(dek []byte, tenantID string) ([]byte, error) {
 	return header, nil
 }
 
-func VerifySignature(dek []byte, header *icl_proto.V3DocumentHeader) bool {
+func verifySignature(dek []byte, header *icl_proto.V3DocumentHeader) bool {
 	if header.GetSaasShield() == nil {
 		return false
 	}
 	headerSig := header.Sig
-	candidateSig, err := NewV3HeaderSignature(headerSig)
+	candidateSig, err := newV3HeaderSignature(headerSig)
 	if err != nil {
 		return false
 	}
-	generatedSign, err := GenerateSignature(dek, candidateSig.nonce, header.GetSaasShield())
+	generatedSign, err := generateSignature(dek, candidateSig.nonce, header.GetSaasShield())
 	if err != nil {
 		return false
 	}
 	return bytes.Equal(generatedSign.tag, candidateSig.tag)
 }
 
-func VerifyPreamble(preamble []byte) bool {
+func verifyPreamble(preamble []byte) bool {
 	return len(preamble) == documentHeaderMetaLength &&
 		preamble[0] == getCurrentDocumentHeaderVersion() &&
 		containsIroncoreMagic(preamble) &&
 		getHeaderSize(preamble) >= 0
 }
 
-func SplitDocument(document []byte) (*DocumentParts, error) {
+func splitDocument(document []byte) (*documentParts, error) {
 	fixedPreamble := document[0:documentHeaderMetaLength]
-	if !VerifyPreamble(fixedPreamble) {
+	if !verifyPreamble(fixedPreamble) {
 		return nil, errors.New("provided bytes were not an IronCore encrypted document")
 	}
 	headerLength := getHeaderSize(fixedPreamble)
 	headerEnd := documentHeaderMetaLength + headerLength
 	header := document[documentHeaderMetaLength:headerEnd]
 	ciphertext := document[headerEnd:]
-	return &DocumentParts{preamble: fixedPreamble, header: header, ciphertext: ciphertext}, nil
+	return &documentParts{preamble: fixedPreamble, header: header, ciphertext: ciphertext}, nil
 
 }
 
@@ -224,23 +224,23 @@ func getCurrentDocumentHeaderVersion() byte {
 	return byte(3)
 }
 
-type V3HeaderSignature struct {
+type v3HeaderSignature struct {
 	tag   []byte
 	nonce []byte
 }
 
-func (s *V3HeaderSignature) GetBytes() []byte {
+func (s *v3HeaderSignature) GetBytes() []byte {
 	return append(s.nonce, s.tag...)
 }
 
-func NewV3HeaderSignature(bytes []byte) (*V3HeaderSignature, error) {
+func newV3HeaderSignature(bytes []byte) (*v3HeaderSignature, error) {
 	if len(bytes) != nonceLen+tagLen {
 		return nil, fmt.Errorf("bytes were not a V3HeaderSignature because their length was %d, not %d", len(bytes), nonceLen+tagLen)
 	}
-	return &V3HeaderSignature{nonce: bytes[0:nonceLen], tag: bytes[nonceLen : nonceLen+tagLen]}, nil
+	return &v3HeaderSignature{nonce: bytes[0:nonceLen], tag: bytes[nonceLen : nonceLen+tagLen]}, nil
 }
 
-type DocumentParts struct {
+type documentParts struct {
 	preamble   []byte
 	header     []byte
 	ciphertext []byte
--- crypto_test.go
+++ crypto_test.go
@@ -1,4 +1,4 @@
-package crypto
+package tsc
 
 import (
 	"crypto/rand"
@@ -36,21 +36,21 @@ func generateDek() []byte {
 func TestEncryptDecryptRoundtrip(t *testing.T) {
 	dek := generateDek()
 	plaintext := []byte("This is a non base64 string.")
-	encryptResult, _ := Encrypt(plaintext, dek)
-	decryptResult, _ := Decrypt(encryptResult, dek)
+	encryptResult, _ := encrypt(plaintext, dek)
+	decryptResult, _ := decrypt(encryptResult, dek)
 	assert.Equal(t, plaintext, decryptResult)
 }
 
 func TestSignVerify(t *testing.T) {
 	dek := generateDek()
 	nonce, _ := generateNonce()
-	proto, _ := CreateHeaderProto(dek, "This is my tenant ID", nonce)
-	assert.True(t, VerifySignature(dek, proto))
+	proto, _ := createHeaderProto(dek, "This is my tenant ID", nonce)
+	assert.True(t, verifySignature(dek, proto))
 }
 
 func TestDecryptingKnownEncryptedValue(t *testing.T) {
 	encryptedDocument, _ := hex.DecodeString(knownGoodEncryptedValueHexString)
-	decryptedBytes, _ := DecryptDocument(encryptedDocument, knownDek)
+	decryptedBytes, _ := decryptDocument(encryptedDocument, knownDek)
 	assert.Equal(t, string(decryptedBytes), "I have a fever and the only cure is nine nine nine nine...")
 }
 
@@ -58,7 +58,7 @@ func TestKnownHeaderProtoFromJava(t *testing.T) {
 	dek, _ := hex.DecodeString("000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F")
 	nonce, _ := hex.DecodeString("3171EF3C899F875E595C2213")
 	expectedHexResult := strings.ToLower("0A1C3171EF3C899F875E595C2213CACF9287C78CF196458CD690544980C71A0A0A0874656E616E744964")
-	result, _ := CreateHeaderProto(dek, "tenantId", nonce)
+	result, _ := createHeaderProto(dek, "tenantId", nonce)
 	resultBytes, _ := proto.Marshal(result)
 	assert.Equal(t, hex.EncodeToString(resultBytes), expectedHexResult)
 }
@@ -67,7 +67,7 @@ func TestKnownHeaderProtoFromJava(t *testing.T) {
 func TestDecryptingBadTag(t *testing.T) {
 	hexString := knownGoodEncryptedValueHexString[0:len(knownGoodEncryptedValueHexString)-2] + "00"
 	encryptedDocument, _ := hex.DecodeString(hexString)
-	_, err := DecryptDocument(encryptedDocument, knownDek)
+	_, err := decryptDocument(encryptedDocument, knownDek)
 	assert.ErrorContains(t, err, "AES decryption failed")
 }
 
@@ -75,7 +75,7 @@ func TestDecryptingBadTag(t *testing.T) {
 func TestDecryptInvalidDocument(t *testing.T) {
 	hexString := "00000000000000"
 	encryptedDocument, _ := hex.DecodeString(hexString)
-	_, err := DecryptDocument(encryptedDocument, knownDek)
+	_, err := decryptDocument(encryptedDocument, knownDek)
 	assert.ErrorContains(t, err, "provided bytes were not an IronCore encrypted document")
 }
 
@@ -83,19 +83,19 @@ func TestDecryptInvalidDocument(t *testing.T) {
 func TestDecryptInvalidDocumentIncorrectLength(t *testing.T) {
 	hexString := "00000000000100" // Length of 256
 	encryptedDocument, _ := hex.DecodeString(hexString)
-	_, err := DecryptDocument(encryptedDocument, knownDek)
+	_, err := decryptDocument(encryptedDocument, knownDek)
 	assert.ErrorContains(t, err, "provided bytes were not an IronCore encrypted document")
 }
 
 func TestVerifyWithWrongDek(t *testing.T) {
 	nonce, _ := generateNonce()
-	header, _ := CreateHeaderProto(knownDek, "tenant", nonce)
-	assert.False(t, VerifySignature(knownDek2, header))
+	header, _ := createHeaderProto(knownDek, "tenant", nonce)
+	assert.False(t, verifySignature(knownDek2, header))
 }
 
 func TestDecryptDocumentWithCorruptHeader(t *testing.T) {
 	corruptDocument, _ := hex.DecodeString(strings.Replace(knownGoodEncryptedValueHexString, "f4e016c0", "00000000", 1))
-	_, err := DecryptDocument(corruptDocument, knownDek)
+	_, err := decryptDocument(corruptDocument, knownDek)
 	assert.ErrorContains(t, err, "provided bytes were not an IronCore encrypted document")
 }
 
@@ -103,14 +103,14 @@ func TestEncryptWithBadNonce(t *testing.T) {
 	plaintext := []byte("This is a non base64 string.")
 	dek := generateDek()
 	badNonce := []byte("foobar")
-	_, err := EncryptWithNonce(plaintext, dek, badNonce)
+	_, err := encryptWithNonce(plaintext, dek, badNonce)
 	assert.ErrorContains(t, err, "the nonce passed was not the correct length")
 }
 
 func TestDecryptTooShort(t *testing.T) {
 	badCiphertext := []byte("foo")
 	dek := generateDek()
-	_, err := Decrypt(badCiphertext, dek)
+	_, err := decrypt(badCiphertext, dek)
 	assert.ErrorContains(t, err, "the ciphertext was not well formed")
 }
 
@@ -118,8 +118,8 @@ func TestRoundtripDocument(t *testing.T) {
 	dek := generateDek()
 	document := []byte("bytes")
 	tenantID := "tenant"
-	encrypted, _ := EncryptDocument(document, tenantID, dek)
-	decrypted, _ := DecryptDocument(encrypted, dek)
+	encrypted, _ := encryptDocument(document, tenantID, dek)
+	decrypted, _ := decryptDocument(encrypted, dek)
 	assert.Equal(t, decrypted, document)
 }
 
@@ -127,22 +127,22 @@ func TestDecryptBadDocument(t *testing.T) {
 	dek := generateDek()
 	document := []byte("bytes")
 	tenantID := "tenant"
-	encrypted, _ := EncryptDocument(document, tenantID, dek)
+	encrypted, _ := encryptDocument(document, tenantID, dek)
 	badDek := []byte("bar")
-	_, err := DecryptDocument(encrypted, badDek)
+	_, err := decryptDocument(encrypted, badDek)
 	assert.ErrorContains(t, err, "the signature computed did not match. The document key is likely incorrect")
 }
 
 func TestGenerateHeaderTooLarge(t *testing.T) {
 	dek := generateDek()
 	tenantID := strings.Repeat("g", 100000)
-	_, err := GenerateHeader(dek, tenantID)
+	_, err := generateHeader(dek, tenantID)
 	assert.ErrorContains(t, err, "the header is too large")
 }
 
 func TestVerifyWrongHeader(t *testing.T) {
 	// No SaasShieldHeader set
 	header := icl_proto.V3DocumentHeader{}
-	verify := VerifySignature([]byte("dek"), &header)
+	verify := verifySignature([]byte("dek"), &header)
 	assert.False(t, verify)
 }
